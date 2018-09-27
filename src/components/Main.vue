6fe4dffc509ea216c67d276b1345da3b9e7e9343
emilundg <lundgrenemil@hotmail.com>
2018-09-27 14:59:10 +0200

    removed redundant

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,31 +1,5 @@
 <template>
   <div id="app">
-    <div>
-      <b-navbar class="b-navbar" toggleable="md" type="dark" variant="dark">
-
-        <b-navbar-brand href="#">ZombiePreventionLab</b-navbar-brand>
-
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-        <b-collapse is-nav id="nav_collapse">
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item class="button" v-bind:active="tab === 1" v-on:click="tab = 1" href="#">Home</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-
-    <b-container v-if="tab === 0" class="container-fluid" fluid>
-      <a-scene embedded arjs='sourceType: webcam;'>
-        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-        <a-marker-camera preset='hiro'></a-marker-camera>
-      </a-scene>
-    </b-container>
-
-    <b-container v-if="tab === 1" class="container-fluid" fluid>
-      <b-card tag="article" class="mb-5">
-        <h2>Random title</h2>
-      </b-card>
-    </b-container>
   </div>
 </template>
 
@@ -35,7 +9,6 @@
     components: {},
     data() {
       return {
-        tab: 0
       }
     },
     created() {},
