62608115d3683ed4baa85ca1c0966877c787df42
emilundg <lundgrenemil@hotmail.com>
2018-09-27 11:22:24 +0200

    added aframe

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,6 +15,12 @@
     </div>
 
     <b-container v-if="tab === 0" class="container-fluid" fluid>
+      <a-scene embedded arjs>
+        <!-- create your content here. just a box for now -->
+        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+        <!-- define a camera which will move according to the marker position -->
+        <a-marker-camera preset='hiro'></a-marker-camera>
+      </a-scene>
     </b-container>
 
     <b-container v-if="tab === 1" class="container-fluid" fluid>
