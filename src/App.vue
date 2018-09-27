1b548823799fb75473934aaeba0af1f30647826e
emilundg <lundgrenemil@hotmail.com>
2018-09-27 11:06:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss">
+  .submit {
+    display: block;
+    margin: 30px auto;
+    height: 38px;
+    padding: 0 30px;
+    line-height: 38px;
+    text-transform: uppercase;
+    background-color: transparent;
+    border-radius: 4px;
+    border: 1px solid #bbb;
+    cursor: pointer;
+    letter-spacing: 0.1rem;
+    transition: all ease-in 0.2s;
+    color: rgba(0, 0, 0, 0.87);
+    outline: none;
+    &:focus {
+      outline: none;
+    }
+    &:hover {
+      border-color: rgb(134, 134, 134);
+    }
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .card {
+    border: none;
+    border-radius: 0px;
+    margin: 4vh;
+    padding: 0px;
+    width: 85vw;
+    min-height: 80vh;
+    box-shadow: 0 3px 8px 0 rgba(68, 45, 45, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.08);
+    h2 {
+      margin-bottom: 20px;
+    }
+  }
+
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+</style>
