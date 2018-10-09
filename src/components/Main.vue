8310750f9fb6f37433244cfa673c4b92df20b220
emilundg <lundgrenemil@hotmail.com>
2018-10-09 11:21:00 +0200

    ok

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,7 +7,7 @@
         <a-text position="0.5 1.2 0" value="here is a hint!"></a-text>
         <!-- use your gltf model -->
         <a-entity scale="0.15 0.15 0.15" gltf-model="url(/static/gltf/dr._plague/scene.gltf)"></a-entity>
-        <!-- <a-entity scale="0.6 0.6 0.6" gltf-model="#ground"></a-entity> -->
+        <a-entity scale="0.6 0.6 0.6" gltf-model="url(/static/gltf/TwoSidedPlane/scene.gltf)"></a-entity>
       </a-marker>
 
       <a-entity camera></a-entity>
