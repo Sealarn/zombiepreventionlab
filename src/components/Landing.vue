535a4e3f72497b54aff1993bb19ac89f98a00177
emilundg <lundgrenemil@hotmail.com>
2018-10-11 13:02:20 +0200

    detect version

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="row" style="justify-content: center;" v-if="filter === 0">
-      <div v-if="filter === 0" class="col-12 col-sm-12 col-md-12 col-lg-12 header mt-5 mb-5">
+    <div class="row" style="justify-content: center;">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 header mt-5 mb-5">
         <p class="header-title font-effect-decaying">Zombie Prevention Lab</p>
       </div>
 
-      <div v-if="filter === 0" class="col-11 col-sm-11 col-md-8 col-lg-6">
+      <div class="col-11 col-sm-11 col-md-8 col-lg-6">
         <b-button class="m-1" v-scroll-to="'#uppdrag'">Skippa backstory</b-button>
 
         <b-card class="envelope m-4" title="Bakgrund" img-src="http://vamers.com/wp-content/uploads/2017/12/Vamers-Gaming-A-New-Walking-Dead-Game-Is-Coming-From-Payday-Developer-Overkill-02.jpg" img-alt="Image" img-top tag="article">
@@ -31,18 +31,17 @@
             <!-- <b-button class="m-1" href="/fuck_filter">Colorblind</b-button>
               <b-button class="m-1" v-bind:active="filter === 2" v-on:click="filter = 2">Monochrome</b-button>
               <b-button class="m-1" v-bind:active="filter === 3" v-on:click="filter = 3">Heatvision</b-button> -->
-            <a href="http://play.google.com/store/apps/details?id=com.google.android.apps.maps"><img class="download" src="/static/images/playstore.svg"></a>
-            <a href="itms://itunes.apple.com/us/app/apple-store/id375380948?mt=8"><img class="download" src="/static/images/app-store.svg"></a>
+            <a v-if="android" href="http://play.google.com/store/apps/details?id=com.google.android.apps.maps"><img class="download" src="/static/images/playstore.svg"></a>
+            <a v-if="!android" href="itms://itunes.apple.com/us/app/apple-store/id375380948?mt=8"><img class="download" src="/static/images/app-store.svg"></a>
           </div>
         </b-jumbotron>
-
       </div>
     </div>
 
-    <div class="row" style="justify-content: center;" v-if="filter === 1">
+    <!-- <div class="row" style="justify-content: center;" v-if="filter === 1">
+      <iframe sclass="col-6 col-sm-6 col-md-6 col-lg-6" allow="vr" src="/ass_filter"></iframe>
       <iframe class="col-6 col-sm-6 col-md-6 col-lg-6" allow="vr" src="/ass_filter"></iframe>
-      <iframe class="col-6 col-sm-6 col-md-6 col-lg-6" allow="vr" src="/ass_filter"></iframe>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -51,10 +50,14 @@
     components: {},
     data() {
       return {
-        filter: 0
+        android: true
       }
     },
-    created() {},
+    created() {
+      if (navigator.platform === 'iPhone') {
+        this.android = false
+      }
+    },
     methods: {}
   }
 </script>
