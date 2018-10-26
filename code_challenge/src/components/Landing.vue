bcadd4c30e399e49fc6b7df3b76b920f796b28f6
emilundg <lundgrenemil@hotmail.com>
2018-10-26 10:30:27 +0200

    changes

--- code_challenge/src/components/Landing.vue
+++ code_challenge/src/components/Landing.vue
@@ -7,12 +7,21 @@
       <a-scene arjs='sourceType: webcam; detectionMode: mono; trackingMethod: best;'>
 
         <!-- here is the marker -->
-        <a-marker preset='hiro'>
+        <a-marker preset='kanji'>
 
           <!-- use your gltf model -->
           <a-entity scale="1 1 1" gltf-model="url(/static/gltf/tiki_treasure/scene.gltf)"></a-entity>
 
         </a-marker>
+
+        <a-marker preset='hiro'>
+
+          <!-- use your gltf model -->
+          <a-entity scale="0.3 0.3 0.3" position="0 0 0" gltf-model="url(/static/gltf/capoeira_oldman/scene.gltf)" animation-mixer="clip: mixamo.com;" fbx-model="src: url(/static/fbx/Capoeira.fbx);"></a-entity>
+          <a-text position="0 3.5 0" value="Det är Fredag mina bekanta!"></a-text>
+
+        </a-marker>
+
 
 
         <a-entity camera></a-entity>
@@ -46,6 +55,9 @@
 
 
 
+<script src="https://github.com/mrdoob/three.js/blob/master/examples/js/libs/inflate.min.js">
+
+</script>
 
 <script>
   export default {
--- code_challenge/src/main.js
+++ code_challenge/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import router from './router';
+require('aframe-extras');
 
 Vue.use(BootstrapVue);
 
